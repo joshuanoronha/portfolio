623acca65d764ce6ab46117a0c41ce4463e96e96
Joshua Noronha <joshuanoronha1304@gmail.com>
2022-12-27 14:21:30 -0700

    Removed comments from CardList

--- src/components/Card/CardList.js
+++ src/components/Card/CardList.js
@@ -23,23 +23,7 @@ export default function CardList() {
       },
     },
   };
-  //   const scroll = window.requestAnimationFrame
-  //         || function (callback) { window.setTimeout(callback, 1000 / 60); };
-  //   function isElementInViewport(el) {
-  //     // special bonus for those using jQuery
-  //     if (typeof jQuery === 'function' && el instanceof jQuery) {
-  //       el = el[0];
-  //     }
-  //     const rect = el.getBoundingClientRect();
-  //     return (
-  //       (rect.top <= 0
-  //                 && rect.bottom >= 0)
-  //             || (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight)
-  //                 && rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-  //             || (rect.top >= 0
-  //                && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  //     );
-  //   }
+
   return (
     <motion.div ref={ref} className="container" variants={cardContainerVariants} initial="hidden" animate={controls}>
       <h2 style={{ color: 'var(--secondary)' }}>A Few of my Projects</h2>
