0a3b3bcad268897549f0c480465bad953d9220ef
Joshua Noronha <joshuanoronha1304@gmail.com>
2022-12-27 14:14:03 -0700

    Added animation on entry in viewport to projects section

--- src/components/Card/CardList.js
+++ src/components/Card/CardList.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 import Card from './Card';
 import { projects } from '../../data/projects';
 
 export default function CardList() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible');
+    }
+  }, [controls, inView]);
   const cardContainerVariants = {
     hidden: {
       opacity: 0,
@@ -33,7 +41,7 @@ export default function CardList() {
   //     );
   //   }
   return (
-    <motion.div className="container" variants={cardContainerVariants} initial="hidden" animate="visible">
+    <motion.div ref={ref} className="container" variants={cardContainerVariants} initial="hidden" animate={controls}>
       <h2 style={{ color: 'var(--secondary)' }}>A Few of my Projects</h2>
       {projects.map((project) => (
         <Card project={project} key={project.name} />
