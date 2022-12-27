50a3f665374b70033bc13410080aa5c2efb31530
Joshua Noronha <joshuanoronha1304@gmail.com>
2022-12-27 14:43:14 -0700

    Added animations to experience part

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 import { experienceList } from '../../data/experience';
 
 export default function Experience() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible');
+    }
+  }, [controls, inView]);
+  const experienceListVariants = {
+    hidden: {
+      opacity: 0,
+      x: 0,
+    },
+    visible: {
+      opacity: 1,
+      x: -10,
+      transition: {
+        staggerChildren: 0.8,
+      },
+    },
+  };
   return (
-    <>
-      <Container style={{ marginTop: '3em' }}>
+    <Container style={{ marginTop: '3em' }}>
+      <motion.div
+        ref={ref}
+        variants={experienceListVariants}
+        initial="hidden"
+        animate={controls}
+      >
         <h1
           style={{
             color: 'var(--secondary)',
@@ -16,12 +43,13 @@ export default function Experience() {
           Experience
         </h1>
         {experienceList.map((experience) => (
-          <span
+          <motion.div
             style={{
               color: 'var(--secondary)',
               marginTop: '1.1em',
             }}
             key={experience.period}
+            variants={experienceListVariants}
           >
             <div style={{ marginTop: '1.5em' }}>
               <span style={{ color: 'var(--fontColor)', fontSize: '1.2rem' }}>
@@ -49,10 +77,10 @@ export default function Experience() {
                 }
               </ul>
             </div>
-          </span>
+          </motion.div>
         ))}
 
-      </Container>
-    </>
+      </motion.div>
+    </Container>
   );
 }
